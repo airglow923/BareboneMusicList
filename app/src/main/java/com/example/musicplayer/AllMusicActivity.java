497a6ac927b9bdfa589c6f0b187be63d541fcc8e
airglow923 <p.hyundeok76@gmail.com>
2020-05-12 17:39:05 +0800

    resolve cropped text

--- app/src/main/java/com/example/musicplayer/AllMusicActivity.java
+++ app/src/main/java/com/example/musicplayer/AllMusicActivity.java
@@ -1,7 +1,6 @@
 package com.example.musicplayer;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
